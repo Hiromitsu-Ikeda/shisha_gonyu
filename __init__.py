478fa9b5b7cb6c47f73b74089dffcb876c9caa89
Hiromitsu-Ikeda <ikd20091001@gmail.com>
2022-08-25 00:58:14 +0900

    [update] explanation of the argument of funcitons

--- __init__.py
+++ __init__.py
@@ -7,8 +7,8 @@ from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
 def shisha_gonyu(value,digit,type_=str,decimal_bool=True):
     """
     ひとつの値だけでなく、配列に関しても四捨五入するための関数
-    value : float, str, list, numpy.ndarrayのいずれかで指定
-    digit : 四捨五入する位or桁数
+    value : float, int, str, list, numpy.ndarrayのいずれかで指定
+    digit : 四捨五入する桁数
     type_ : 出力する際のデータ型 (ex. float, int)
     decimal_bool : 小数点以下の四捨五入か否か。Falseの場合は整数桁に関して四捨五入が行われる。
     """        
@@ -40,7 +40,7 @@ def one_decimal(value,digit,type_=str):
     """
     ひとつの値に関して小数点以下を四捨五入するための関数
     value : float, str, のいずれかで指定
-    digit : 小数点以下の桁数
+    digit : 四捨五入した結果の小数点以下の桁数　(整数->0, 小数点第一位->1, 小数点第二位->2,...)
     type_ : 出力する際のデータ型 (ex. float, int)
     """        
     if digit==0:
@@ -56,8 +56,8 @@ def one_decimal(value,digit,type_=str):
 
 def one_integer(value,digit,type_=str):
     """
-    ひとつの値に関して整数桁を四捨五入するための関数
-    value : float, str, のいずれかで指定
+    ひとつの値に関して整数の各位で四捨五入するための関数
+    value : float, int, str のいずれかで指定
     digit : 四捨五入する位の数 (1の位->1, 10の位->2, ...)
     type_ : 出力する際のデータ型 (ex. float, int)
     """        
